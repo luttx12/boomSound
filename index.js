3bc0b1a6314df45ca88efa513e0ea1bb3035561f
luttx12 <luttx12@gmail.com>
2023-08-18 19:25:58 +0800

    boomSound website

---
+++ index.js
@@ -0,0 +1,11 @@
+let boomSound = document.getElementById("boomsound");
+
+function playBoomSound()
+{
+    boomSound.currentTime = 1;
+    boomSound.play();
+}
+
+function openPopup(){
+    window.open('https://www.facebook.com/luttx12',"","width=100,heigth=100");
+}
